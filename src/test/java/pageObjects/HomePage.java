7fdff579253d9c029da8403cb9ddae8afd5ce5d2
Bhagwat <bhagwat22garibe@gmail.com>
2024-09-16 19:41:15 +0530

    test commit

--- src/test/java/pageObjects/HomePage.java
+++ src/test/java/pageObjects/HomePage.java
@@ -1,11 +1,17 @@
 package pageObjects;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HomePage extends BasePage{
 
+	WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+	
 	public HomePage(WebDriver driver) {
 		super(driver);
 	}
@@ -20,7 +26,8 @@ public class HomePage extends BasePage{
 	WebElement lnkLogin;
 	
 	public void clickOnMyAccount() {
-		lnkMyAccount.click();
+		wait.until(ExpectedConditions.elementToBeClickable(lnkMyAccount));
+		lnkMyAccount.click();		;
 	}
 	
 	public void clickOnRegister() {
