7e5f190ae1d012093af098972997c09c73b1656b
Bhagwat <bhagwat22garibe@gmail.com>
2024-09-16 18:01:49 +0530

    changed expath for My Account page heading

--- src/test/java/pageObjects/MyAccountPage.java
+++ src/test/java/pageObjects/MyAccountPage.java
@@ -5,12 +5,12 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class MyAccountPage extends BasePage {
-
+	
 	public MyAccountPage(WebDriver driver) {
 		super(driver);
 	}
 
-	@FindBy(xpath = "//h2[text()='My Account']") // MyAccount Page heading
+	@FindBy(xpath = "//h2[normalize-space()='My Account']") // MyAccount Page heading
 	WebElement msgHeading;
 
 	@FindBy(xpath = "//div[@class='list-group']//a[text()='Logout']")
